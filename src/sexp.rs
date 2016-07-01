36ceadcf041be8e5f2a3654dc4c2d67f24f5e118
Drew Chandler <drew@kongregate.com>
2016-07-01 14:24:10 -0700

    Implement Display for Sexp

--- src/sexp.rs
+++ src/sexp.rs
@@ -1,7 +1,27 @@
+use std::fmt;
+
 #[derive(PartialEq, Debug)]
 pub enum Sexp {
     Number(f64),
     String(String),
     Symbol(String),
     List(Vec<Sexp>),
+}
+
+impl fmt::Display for Sexp {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            Sexp::Number(ref n) => write!(f, "{}", n),
+            Sexp::String(ref s) => write!(f, "\"{}\"", s),
+            Sexp::Symbol(ref s) => write!(f, "{}", s),
+            Sexp::List(ref v) => {
+                try!(write!(f, "("));
+                for (i, s) in v.iter().enumerate() {
+                    if i > 0 { try!(write!(f, " ")); }
+                    try!(write!(f, "{}", s));
+                }
+                write!(f, ")")
+            }
+        }
+    }
 }
