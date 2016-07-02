902601321bf93acce833ca98b1d35db068cff6f0
Drew Chandler <andrewschandler@gmail.com>
2016-07-02 16:27:08 -0700

    Create a REPL

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,34 @@
 #[macro_use]
 extern crate nom;
+extern crate rustyline;
+
+use rustyline::Editor;
+use nom::IResult::Done;
 
 mod sexp;
 mod parser;
 
 fn main() {
-    println!("Hello, world!");
+    let mut rl = Editor::new();
+    let _ = rl.load_history("history.txt");
+
+    loop {
+        let readline = rl.readline("rl> ");
+        match readline {
+            Ok(line) => {
+                rl.add_history_entry(&line);
+
+                match parser::sexp(line.as_bytes()) {
+                    Done(_, s) => println!("{}", s),
+                    _ => println!("Error!")
+                };
+            },
+            _ => {
+                println!("exiting...");
+                break;
+            }
+        }
+    }
+
+    rl.save_history("history.txt").unwrap();
 }
