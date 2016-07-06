502879ebaa6ba45b21f37bd9b401f773b99a3681
Drew Chandler <andrewschandler@gmail.com>
2016-07-06 12:44:06 -0700

    Add built-in functions for subtraction and multiplication

--- src/built_in.rs
+++ src/built_in.rs
@@ -2,11 +2,30 @@ use super::env::{self, Env};
 use super::sexp::{Sexp, SexpResult};
 
 macro_rules! unpack_args {
-    ($src:expr, => $t:path) => {{
+    ($src:expr, N $rest:path) => {{
+        unpack_arg!($src, 0usize, N $rest)
+    }};
+
+    ($src:expr, $(1 $t:path),+, N $rest:path) => {{
+        let mut index = 0usize;
+
+        (
+            $({
+                index += 1;
+                unpack_arg!($src, index - 1, 1 $t)
+            }),*,
+            unpack_arg!($src, index, N $rest)
+        )
+    }};
+}
+
+macro_rules! unpack_arg {
+    ($src:expr, $index:expr, N $rest:path) => {{
         let tmp: Result<Vec<_>, String> = $src.iter()
+            .skip($index)
             .map(|i| {
                 match *i {
-                    $t(ref n) => Ok(*n),
+                    $rest(ref n) => Ok(*n),
                     ref v @ _ => Err(format!("Argument error: {}", v)),
                 }
             })
@@ -15,18 +34,49 @@ macro_rules! unpack_args {
         if let Err(m) = tmp { return Err(m) }
 
         tmp.unwrap()
-    }}
+    }};
+
+    ($src:expr, $index:expr, 1 $t:path) => {{
+        let tmp = $src.iter().nth($index);
+
+        if tmp.is_none() {
+            return Err(format!("Invalid number of arguments: {}", $src.iter().len()))
+        }
+
+        match *tmp.unwrap() {
+            $t(ref n) => *n,
+            ref v @ _ => return Err(format!("Argument error: {}", v)),
+        }
+    }};
 }
 
 fn add(args: Vec<Sexp>) -> SexpResult {
-    let ns = unpack_args!(args, => Sexp::Number);
+    let ns = unpack_args!(args, N Sexp::Number);
 
     Ok(Sexp::Number(ns.iter().fold(0., |sum, n| sum + *n)))
+}
+
+fn subtract(args: Vec<Sexp>) -> SexpResult {
+    let (first, rest) = unpack_args!(args, 1 Sexp::Number, N Sexp::Number);
+
+    if rest.len() == 0 {
+        Ok(Sexp::Number(0. - first))
+    } else {
+        Ok(Sexp::Number(rest.iter().fold(first, |result, n| result - *n)))
+    }
+}
+
+fn multiply(args: Vec<Sexp>) -> SexpResult {
+    let (first, rest) = unpack_args!(args, 1 Sexp::Number, N Sexp::Number);
+
+    Ok(Sexp::Number(rest.iter().fold(first, |result, n| result * *n)))
 }
 
 pub fn default_env() -> Env {
     let env = env::env_new(None);
     env::env_set(&env, "+".to_string(), Sexp::BuiltInFunc(add));
+    env::env_set(&env, "-".to_string(), Sexp::BuiltInFunc(subtract));
+    env::env_set(&env, "*".to_string(), Sexp::BuiltInFunc(multiply));
 
     env
 }
@@ -41,6 +91,29 @@ mod tests {
         assert_eq!(super::add(vec![Sexp::Number(1.), Sexp::Number(2.), Sexp::Number(3.)]),
                    Ok(Sexp::Number(6.)));
         assert_eq!(super::add(vec![Sexp::Number(1.),
+                                   Sexp::Number(2.),
+                                   Sexp::String("3".to_string())]),
+                   Err("Argument error: \"3\"".to_string()));
+    }
+
+    #[test]
+    fn test_subtract() {
+        assert_eq!(super::subtract(vec![Sexp::Number(1.)]), Ok(Sexp::Number(-1.)));
+        assert_eq!(super::subtract(vec![Sexp::Number(1.), Sexp::Number(2.), Sexp::Number(3.)]),
+                   Ok(Sexp::Number(-4.)));
+        assert_eq!(super::subtract(vec![Sexp::Number(1.),
+                                   Sexp::Number(2.),
+                                   Sexp::String("3".to_string())]),
+                   Err("Argument error: \"3\"".to_string()));
+        assert_eq!(super::subtract(vec![]), Err("Invalid number of arguments: 0".to_string()));
+    }
+
+    #[test]
+    fn test_multiply() {
+        assert_eq!(super::multiply(vec![Sexp::Number(1.)]), Ok(Sexp::Number(1.)));
+        assert_eq!(super::multiply(vec![Sexp::Number(1.), Sexp::Number(2.), Sexp::Number(3.)]),
+                   Ok(Sexp::Number(6.)));
+        assert_eq!(super::multiply(vec![Sexp::Number(1.),
                                    Sexp::Number(2.),
                                    Sexp::String("3".to_string())]),
                    Err("Argument error: \"3\"".to_string()));
